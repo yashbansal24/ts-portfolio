70e0a3daecec867003c2ce08fdd393cbf8fd0ff4
Yash Bansal <yashbansal97@gmail.com>
2023-03-01 23:44:05 +0530

    add portfolio details

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,6 @@
 import { Modal, useModal, Button, Text, Spacer } from "@nextui-org/react";
 import { FaNewspaper } from 'react-icons/fa';
+import { SiStorybook } from 'react-icons/si';
 
 export default function Portfolio(props) {
   return (
@@ -13,10 +14,13 @@ export default function Portfolio(props) {
       >
         <Modal.Header>
           <Text id="modal-title" size={18} style={{'marginRight': '10px'}}>
-            Patents  
+            Patents
           </Text>
           <FaNewspaper size={30}/>
-          
+          <Text id="modal-title" size={18} style={{'margin': '0 10px'}}>
+            & Portfolio
+          </Text>
+          <SiStorybook size={30}/>
         </Modal.Header>
         <Modal.Body>
         <Text h4 id="modal-description">
@@ -30,6 +34,20 @@ export default function Portfolio(props) {
         </Text>
         <Text h6 id="modal-description">
             - US 10956698.
+        </Text>
+        <Text h4 id="modal-description">
+          PayPal Invoicing
+        </Text>
+          <Text h6 id="modal-description">
+            - Built various features for the webapp. Full details in the Introduction.
+            <Spacer/>
+            - Built invoice generator page for SEO.
+          </Text>
+          <Text h4 id="modal-description">
+            H1DN Schema Transformer
+        </Text>
+        <Text h6 id="modal-description">
+            - An open source attempt to transform any data to any other format.
         </Text>
         </Modal.Body>
         <Modal.Footer>
