1be65da140db752f48a6fb6ab3401647feabd1cd
Yash Bansal <yashbansal97@gmail.com>
2023-03-08 23:38:00 +0530

    404 update

--- src/404.tsx
+++ src/404.tsx
@@ -1,5 +1,10 @@
 import { TbError404 } from 'react-icons/tb'
 
 export default function () {
-    return <div style={{'display': 'flex'}}><TbError404 size={200} style={{'margin': '0 auto'}}></TbError404></div>;
+    return (
+    <div style={{'display': 'flex'}}>
+        <TbError404 size={200} style={{'margin': '0 auto'}}>
+        </TbError404>
+        404 Not Found
+    </div>);
 }
