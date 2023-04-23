4bf5e008686a365c274d6cac1a9570e804fdefe6
Yash Bansal <yashbansal97@gmail.com>
2023-04-23 23:28:26 +0530

    add chatgpt prompt engineer content

--- src/chatgpt.tsx
+++ src/chatgpt.tsx
@@ -8,12 +8,17 @@ export default function Backend() {
   return (
     <React.Fragment>
     <Navigation />
-      <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Chat GPT Prompt 1 </Text>
-      <Spacer />
-      <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Chat GPT Prompt 2</Text>
-      <Spacer />
-      <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Node JS Prompt engineer</Text>
-      <Spacer />
-    </React.Fragment>
+    <Text css={{'textAlign': 'center', 'margin': '20px 0'}}>
+        First invented by Github Copilot, prompt engineering is the process of using tools like chatGPT or Copilot to generate code snippets based on a given prompt.
+        It allows us to quickly prototype and iterate on different ideas, saving us time and effort in the development process.
+        For those who are not familiar with chatGPT or Github Co-pilot, they are state-of-the-art language models that can generate human-like text based on a given prompt. It has been a popular way to generate code snippets that can be built upon and incorporated into projects. 
+    </Text>
+    <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Chat GPT Prompt 1 </Text>
+    <Spacer />
+    <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Chat GPT Prompt 2</Text>
+    <Spacer />
+    <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Node JS Prompt engineer</Text>
+    <Spacer />
+  </React.Fragment>
   );
 }
