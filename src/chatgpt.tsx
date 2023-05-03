4f621582a30e1591363318b7500c865db61d76d9
Yash Bansal <yashbansal97@gmail.com>
2023-05-03 22:38:49 +0530

    add chatgpt prompt engineer content links

--- src/chatgpt.tsx
+++ src/chatgpt.tsx
@@ -13,9 +13,9 @@ export default function Backend() {
         It allows us to quickly prototype and iterate on different ideas, saving us time and effort in the development process.
         For those who are not familiar with chatGPT or Github Co-pilot, they are state-of-the-art language models that can generate human-like text based on a given prompt. It has been a popular way to generate code snippets that can be built upon and incorporated into projects. 
     </Text>
-    <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Careers in AI </Text>
+    <Text h3 css={{'textAlign': 'center', 'margin': '20px 0'}}> Careers in AI </Text>
     <Spacer />
-    <Text h4 css={{'textAlign': 'center', 'margin': '20px 0'}}> Node JS Prompt engineer</Text>
+    <Text h3 css={{'textAlign': 'center', 'margin': '20px 0'}}> Node JS Prompt engineer</Text>
     <Link href="https://www.deeplearning.ai/short-courses/chatgpt-prompt-engineering-for-developers/"> New Prompt Engineering course </Link>
     <Spacer />
   </React.Fragment>
